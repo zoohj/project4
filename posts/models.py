45dc3b7669b848a97d575a9662437c3c49ae740b
주현지 <hyjoo0909@naver.com>
2023-09-26 20:41:13 +0900

    feat: users, posts model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    # type= 신고 or 신청
+
+    title = models.CharField(max_length= 20)
+    content = models.TextField(max_length=300, default='')
+    image= models.ImageField(upload_to="post", null= True)
+    
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add= True, blank= True)
+
+    likes = models.ManyToManyField(User,related_name = "like_post")
+    
+    def get_likes(self):
+        return self.likes.all().count()
