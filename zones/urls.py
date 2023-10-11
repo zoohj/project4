00b044f92dd92ed03febec2e4d5ac1dd0d7ffdd2
Seun9w00 <129683560+BaekSeungW00@users.noreply.github.com>
2023-10-12 01:45:41 +0900

    feat: Add urls of review

--- zones/urls.py
+++ zones/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.SmokingZoneCreateListAPIView.as_view()),
-    path('/<id:smoking_zone_id>/', views.SmokingZoneRetrieveUpdateDestroyAPIView.as_view()),
+    path('', views.SmokingZoneCreateListAPIView.as_view()),
+    path('<id:smoking_zone_id>/', views.SmokingZoneRetrieveUpdateDestroyAPIView.as_view()),
+    path('review/', views.ReviewCreateAPIView.as_view()),
+    path('review/<int:review_id>/', views.ReviewDestroyAPIView.as_view()),
 ]
