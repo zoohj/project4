2dda70103333fc3ef7dccb30e7d80df0e61a46cc
주현지 <hyjoo0909@naver.com>
2023-10-12 01:36:36 +0900

    feat: post views 생성

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,35 @@
+from http import HTTPStatus
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+
+from posts.models import Post
+from posts.serializers import PostSerializer
+from rest_framework import generics, status
+
+
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post
+    serializer_class = PostSerializer
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post
+    serializer_class= PostSerializer
+
+class PostMostView(generics.ListAPIView):
+    queryset = Post.objects.all.order_by('-likes')
+    serializer_class= PostSerializer
+
+class PostRecentView(generics.ListAPIView):
+    queryset = Post.objects.all.order_by('-created_at')
+    serializer_class= PostSerializer
+
+class PostDeleteView(generics.DestroyAPIView):
+    queryset = Post
+    serializer_class = PostSerializer
+    def delete(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return Response(status= status.HTTP_403_FORBIDDEN)
+        return self.destroy(request, *args, **kwargs)
